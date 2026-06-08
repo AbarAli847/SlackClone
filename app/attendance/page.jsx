2244d3967597f2f0eff4acded1ea41d9feb7204a
ABRAR ALi <abraraliengineer@gmail.com>
2026-06-08 13:04:36 +0500

    create attendec page for link all pages

---
+++ app/attendance/page.jsx
@@ -0,0 +1,8 @@
+'use client';
+
+import React from 'react';
+import AttendanceDashboard from '../components/Attendance/AttendanceDashboard';
+
+export default function AttendancePage() {
+  return <AttendanceDashboard />;
+}
